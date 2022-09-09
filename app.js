ee53b2162976c37b8e8803e738b094180efd3ac2
MANU BHARADWAJ <pandatmanu318@gmail.com>
2022-09-09 09:52:14 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+function lighton() {
+    document.getElementById( 'myimage' ).src = 'images/bulbon.png';
+}
+function lightoff() {
+    document.getElementById( 'myimage' ).src = 'images/bulboff.jpg';
+}
